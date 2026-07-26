b23889d2b8942f539ef234aa14ff14a01ad3fc23
0x1 <kevin.francart3@gmail.com>
2026-07-26 14:40:25 +0200

    feat: add C# FFI implementations

---
+++ src/Effect/Ref.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Effect.Ref;
+
+public static class FFI {
+    public static object _New(object arg1) => throw new NotImplementedException("Not implemented: _New");
+    public static object NewWithSelf(object arg1) => throw new NotImplementedException("Not implemented: NewWithSelf");
+    public static object Read(object arg1) => throw new NotImplementedException("Not implemented: Read");
+    public static object ModifyImpl(object arg1) => throw new NotImplementedException("Not implemented: ModifyImpl");
+    public static object Write(object arg1, object arg2) => throw new NotImplementedException("Not implemented: Write");
+}
